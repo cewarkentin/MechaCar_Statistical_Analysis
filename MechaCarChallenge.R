ac6390e49b5eba91bb210500340fb1db1f01eee3
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-08-10 14:55:19 -0500

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,15 @@ summary(multi.fit)
 suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- suspension_table%>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 lot_summary <- suspension_table%>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# D3
+t.test(suspension_table$PSI, mu = 1500 )
+
+Lot_1_table <- suspension_table%>% filter(Manufacturing_Lot == "Lot1")
+t.test(Lot_1_table$PSI, mu = 1500)
+
+Lot_2_table <- suspension_table%>% filter(Manufacturing_Lot == "Lot2")
+t.test(Lot_2_table$PSI, mu = 1500)
+
+Lot_3_table <- suspension_table%>% filter(Manufacturing_Lot == "Lot3")
+t.test(Lot_3_table$PSI, mu = 1500)
