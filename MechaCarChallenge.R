2a5165039bc3128d6a0fbe7f1cf35538c8e1b94c
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-08-10 15:03:17 -0500

    Images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(dplyr)
 
 # D1
-
 mechacar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 multi.fit = lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mechacar_table)
 summary(multi.fit)
