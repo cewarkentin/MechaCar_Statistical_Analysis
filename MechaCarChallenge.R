0e98eb8acc77a13f6eb9263d5685147ca1d4e917
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-08-10 14:51:28 -0500

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,6 @@ multi.fit = lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+
 summary(multi.fit)
 
 #D2
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension_table%>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+lot_summary <- suspension_table%>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
